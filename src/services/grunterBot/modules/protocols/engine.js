46067040953e95adab20acb9ae621ebe3285481d
Alfaro Sierra <manuel.alfaro-sierra-external@capgemini.com>
2017-03-29 13:07:47 +0100

    Developing...

--- src/services/grunterBot/modules/protocols/engine.js
+++ src/services/grunterBot/modules/protocols/engine.js
@@ -21,38 +21,51 @@ class Engine extends Events {
         return false;
     }
 
+    _resolveCommand(script, value) {
+        if (script.next === 'matcher') {
+            this.script = (Array.isArray(this.scripts[value])) ? Object.assign(this.script, this.scripts[value][0]) : Object.assign(this.script, this.scripts[value]);
+            this.script.name = value;
+        } else if (script.next === 'question') {
+            let index = _.findIndex(this.scripts[script.name], {
+                question: script.question
+            });
+            if (index < this.scripts[script.name].length) {
+                this.script = Object.assign(this.script, this.scripts[script.name][index + 1]);
+                this.script.name = script.name;
+            }
+        } else if (script.next === 'ayuda') {
+            this.script = Object.assign({}, this.scripts.ayuda);
+            this.script.name = script.next;
+        } else {
+            console.log('Something go wrong, "next" action %s no allowed', script.next);
+        }
+    }
+
+    _extractCommand(response) {
+        let script, value;
+
+        let match = response.match(new RegExp(this.script.matcher, 'i'));
+        value = match[0].trim().toLowerCase();
+        if (!value) {
+            throw new Error();
+        }
+
+        script = Object.assign({}, this.script);
+        this.script = {};
+        this.values[this.script.valueName] = value;
+
+        return {
+            'script': script,
+            'value': value
+        };
+    }
+
     _processResponse(response) {
         let script = {};
         try {
             if (this._checkAyuda(response)) return;
-            let match = response.match(new RegExp(this.script.matcher, 'i'));
-            let value = match[0].trim().toLowerCase();
-            if (!value) {
-                throw new Error();
-            }
-
-            script = Object.assign({}, this.script);
-            this.script = {};
-            this.values[this.script.valueName] = value;
-
-            if (script.next === 'matcher') {
-                this.script = (Array.isArray(this.scripts[value])) ? Object.assign(this.script, this.scripts[value][0]) : Object.assign(this.script, this.scripts[value]);
-                this.script.name = value;
-            } else if (script.next === 'question') {
-                let index = _.findIndex(this.scripts[script.name], {
-                    question: script.question
-                });
-                if (index < this.scripts[script.name].length) {
-                    this.script = Object.assign(this.script, this.scripts[script.name][index + 1]);
-                    this.script.name = script.name;
-                }
-            } else if (script.next === 'ayuda') {
-                this.script = Object.assign({}, this.scripts.ayuda);
-                this.script.name = script.next;
-            } else {
-                console.log('Something go wrong, "next" action %s no allowed', script.next);
-            }
-
+            let [script, value] = this._extractCommand(response);
+            this._resolveCommand(script, value);
         } catch (err) {
             console.log('regex: %s', this.script.matcher);
             console.log('string: %s', response);
@@ -77,35 +90,4 @@ class Engine extends Events {
     }
 }
 
-// TEST
-const readline = require('readline');
-let events = new Events();
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-const engine = new Engine();
-
-function _handleResponse(response) {
-    engine.next(response);
-}
-
-function _handleQuestionReady(question) {
-    rl.question(question + ' > ', _handleResponse.bind());
-}
-
-
-function _handleFinishApk(values) {
-    console.log('finish apk %s', JSON.stringify(values));
-}
-
-function _handleFinishPdf(values) {
-    console.log('finish pdf %s', JSON.stringify(values));
-}
-
-
-engine.on('script:question:ready', _handleQuestionReady);
-engine.on('script:apk:finish', _handleFinishApk);
-engine.on('script:pdf:finish', _handleFinishPdf);
-engine.start();
+module.export = Engine;
---
+++ src/services/grunterBot/modules/protocols/testConsole.js
@@ -0,0 +1,42 @@
+'use strict'
+
+const readline = require('readline');
+const Engine = require('./engine');
+
+class Test {
+    run() {
+        // TEST
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+
+        const engine = new Engine();
+
+        function _handleResponse(response) {
+            engine.next(response);
+        }
+
+        function _handleQuestionReady(question) {
+            rl.question(question + ' > ', _handleResponse.bind());
+        }
+
+
+        function _handleFinishApk(values) {
+            console.log('finish apk %s', JSON.stringify(values));
+        }
+
+        function _handleFinishPdf(values) {
+            console.log('finish pdf %s', JSON.stringify(values));
+        }
+
+
+        engine.on('script:question:ready', _handleQuestionReady);
+        engine.on('script:apk:finish', _handleFinishApk);
+        engine.on('script:pdf:finish', _handleFinishPdf);
+        engine.start();
+
+    }
+}
+
+module.exports = Test;
